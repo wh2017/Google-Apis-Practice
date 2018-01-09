964e11f871513886755c30814d7651547ebb81eb
wh2017 <hong.wei@daocloud.io>
2018-01-09 10:57:07 +0800

    A 增加 增加点击列表视图时，相应的marker出现动画 A 增加 增加jquery，增加第三方API请求 A 增加 增加添加筛选条件后显示筛选后的地点子集

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var map;
+var map, defaultIcon, defaultIcon;
 var markers = [];
 var initLocations = [
   {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
@@ -11,8 +11,8 @@ var initLocations = [
 
 // 初始化的js函数加载地图
 function initMap() {
-  var largeInfowindow = new google.maps.InfoWindow();
-  var defaultIcon = makeMarkerIcon('0091ff');
+  largeInfowindow = new google.maps.InfoWindow();
+  defaultIcon = makeMarkerIcon('0091ff');
   var highlightedIcon = makeMarkerIcon('FFFF24');
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 40.7413549, lng: -73.9980244},
@@ -26,38 +26,48 @@ function initMap() {
     var marker = new google.maps.Marker({ // 使用google.maps.Marker创建一个新的标记实例
       position: item.location, // 标记显示的位置
       map: map, // 标记显示的地图
-      title: item.title,  // 标记的名称，鼠标悬浮在marker上使显示
+      title: item.title,
       icon: defaultIcon
     });
     markers.push(marker);
     marker.addListener('click', function() {
       marker.setIcon(highlightedIcon);
       marker.setAnimation(google.maps.Animation.BOUNCE);
+      // 2 秒钟后停止动画
+      window.setTimeout(function(){
+        marker.setAnimation(null);
+      }, 2000);
       populateInfoWindow(this, largeInfowindow);
     });
   })
+  
   // 打开信息窗体，同时关闭其余已经打开的信息窗体
   function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      // Clear the infowindow content to give the streetview time to load.
-      infowindow.setContent('');
-      if (infowindow.marker) {
-        infowindow.marker.setIcon(defaultIcon);
-        infowindow.marker.setAnimation(null);
-      }
-      infowindow.marker = marker;
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick', function() {
+    var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + marker.title + '&sort=newest&api-key=f4f76e351f2f4625a6e9190c3c49d40d';
+    $.getJSON(url, function(data) {
+      var items = [];
+      // Check to make sure the infowindow is not already opened on this marker.
+      if (infowindow.marker != marker) {
+        // Clear the infowindow content to give the streetview time to load.
+        infowindow.setContent('');
         if (infowindow.marker) {
           infowindow.marker.setIcon(defaultIcon);
-          infowindow.marker.setAnimation(null);
         }
-        infowindow.marker = null;
-      });
-      infowindow.setContent(marker.title);
-      infowindow.open(map, marker);
-    }
+        infowindow.marker = marker;
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function() {
+          if (infowindow.marker) {
+            infowindow.marker.setIcon(defaultIcon);
+          }
+          infowindow.marker = null;
+        });
+        infowindow.setContent('<li class=article><a href="' + data.response.docs[0].web_url + '" target=_blank>' + data.response.docs[0].headline.main + '</a></li>');
+        infowindow.open(map, marker);
+      }
+    })
+    .error(function(error) {
+      window.alert(error.responseText || 'We did not find any article matching this marker!');
+    });
   }
 
   // 更改marker颜色
@@ -76,19 +86,34 @@ function initMap() {
 var viewModel = function() {
   var self = this;
   var hasMarker = false;
-  inputValue = ko.observable();
-  self.searchAddress = function() {
-    var address = self.inputValue();
-    console.log(self.inputValue());
+  self.inputValue = ko.observable();
+  self.filterValue = ko.observable();
+  self.filterArr = ko.computed(function() {
+    if (!self.filterValue()) {
+      return initLocations;
+    } else {
+      return initLocations.filter(function(item) {
+        return item.title.toLowerCase().indexOf(self.filterValue().toLowerCase()) != -1;
+      });
+    }
+  });
+  // 点击筛选器或者输入地点筛选
+  self.searchAddress = function(event) {
+    var address = event.title || self.inputValue();
     if (!address) {
       window.alert('You must enter an area, or address.');
     } else {
       hasMarker = false;
-      initLocations.forEach(function(item, index) {
-        if(item.title.indexOf(address) < 0) {
+      self.filterArr().forEach(function(item, index) {
+        if(item.title.toLowerCase().indexOf(address.toLowerCase()) < 0) {
           markers[index].setMap(null)
         } else {
           hasMarker = true;
+          markers[index].setAnimation(google.maps.Animation.BOUNCE);
+          // 2 秒钟后停止动画
+          window.setTimeout(function(){
+            markers[index].setAnimation(null);
+          }, 2000);
           markers[index].setMap(map);
         }
       })
@@ -96,7 +121,26 @@ var viewModel = function() {
     // 找不到地点时
     if (!hasMarker) {
       window.alert('We did not find any places matching that search in initLocations!');
+      initMap();
     }
   }
+  // // 输入筛选的地点时
+  // self.filterAddress = function() {
+  //   console.log(11, self.filterValue());
+  //   if (self.filterValue()) {
+  //     self.initLocations.forEach(function(key, index){
+  //       if (key.title.indexOf(self.filterValue()) >= 0) {
+  //         self.filterArr().push(self.initLocations[index])
+  //       }
+  //     })
+  //   } else {
+  //     self.filterArr = ko.observable(initLocations);
+  //   }
+  // }
+  // self.leaveFliter = function() {
+  //   if (!self.filterValue()) {
+  //     self.filterArr = ko.observable(initLocations);
+  //   }
+  // }
 }
 ko.applyBindings(new viewModel());
